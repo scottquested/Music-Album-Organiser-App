8f4440d74d3567317204292e092d30e3ed868ba5
Scott <me@scottquested.com>
2021-02-16 21:48:14 +0000

    SQ Update public path so Github can find assets.

--- webpack.config.js
+++ webpack.config.js
@@ -112,7 +112,7 @@ module.exports = {
 	output: {
 		path: path.resolve(__dirname, 'docs'),
 		pathinfo: false,
-		publicPath: '/',
+		publicPath: 'Music-Album-Organiser-App/',
 		filename: env === 'production' ? 'js/min/[name].bundle.min.js?[contenthash:8]' : 'js/[name].bundle.js'
 	}
 }
