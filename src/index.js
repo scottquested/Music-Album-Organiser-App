d9c28ad84c4ec4b6cb533af70e4f28dab2ce4b57
Scott <me@scottquested.com>
2021-02-17 09:42:37 +0000

    SQ Remove font files from production folder. Fix alignment of filter on pagination.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import 'bootstrap-css-only/css/bootstrap.min.css'
-import 'font-awesome/css/font-awesome.min.css'
 import './scss/main.scss'
 
 import './js/main-script'
--- webpack.config.js
+++ webpack.config.js
@@ -86,19 +86,19 @@ module.exports = {
 					'css-loader'
 				]
 			},
-			{
-				test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-				use: [
-					{
-						loader: 'file-loader',
-						options: {
-							name: '[name].[ext]',
-							outputPath: 'docs/fonts',
-							publicPath: '../../../docs/fonts'
-						}
-					}
-				]
-			}
+			// {
+			// 	test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+			// 	use: [
+			// 		{
+			// 			loader: 'file-loader',
+			// 			options: {
+			// 				name: '[name].[ext]',
+			// 				outputPath: 'docs/fonts',
+			// 				publicPath: '../../../Music-Album-Organiser-App/docs/fonts'
+			// 			}
+			// 		}
+			// 	]
+			// }
 		]
 	},
 	plugins: [
